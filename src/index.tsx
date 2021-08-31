50f030fde31795ec0af84fa5616e57382d5cfe39
zhousiping <1720557879@qq.com>
2021-08-31 14:33:23 +0800

    ci: scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Router from './router';
 import reportWebVitals from './reportWebVitals';
+import './style/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
