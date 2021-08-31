e46a288b5aca11d39ee0b027cd2b8b05e498cd58
zhousiping <1720557879@qq.com>
2021-08-31 14:19:30 +0800

    ci: Add formatter prettier

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="Home"><h1>Home</h1></div>
+    <div className="Home">
+      <h1>Home</h1>
+    </div>
   );
 }
 
--- src/router.tsx
+++ src/router.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/home';
 import About from './pages/about';
 
-
 function Router() {
   return (
     <BrowserRouter>
