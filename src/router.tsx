bb20e66ccc3c23c957e4adbe8a361cc298e6f3ba
zhousiping <1720557879@qq.com>
2021-08-31 11:33:34 +0800

    ci: 添加router

---
+++ src/router.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
