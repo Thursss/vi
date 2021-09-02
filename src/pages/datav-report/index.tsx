0027c9b3c6709f339fb9785103931160ca6bd097
zhousiping <1720557879@qq.com>
2021-09-02 14:54:27 +0800

    ci: styled-components

--- src/pages/datav-report/index.tsx
+++ src/pages/datav-report/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyleDashboard = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  overflow: auto;
+`;
 
 function DatavReport() {
   return (
-    <div className="dashboard">
-      <main>
-        <h1>data-report</h1>
-      </main>
-    </div>
+    <StyleDashboard className="dashboard">
+      <h1>data-report</h1>
+    </StyleDashboard>
   );
 }
 
