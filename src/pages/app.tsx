0dcc6cb47a170b570a10a68cf29a7341c1f89460
zhousiping <1720557879@qq.com>
2021-09-10 10:42:27 +0800

    feat: wecome页美化

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <StyleApp className="App">
+      <StyleCenter>Welcome to ZI</StyleCenter>
+    </StyleApp>
   );
 }
 
+const StyleApp = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #1fc8db;
+  background-image: linear-gradient(
+    141deg,
+    #9fb8ad 0%,
+    #1fc8db 51%,
+    #2cb5e8 75%
+  );
+`;
+const StyleCenter = styled.h1`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 82px;
+  color: #444;
+`;
 export default App;
